1e27072199970bd15efb9efd7c89c492e5a10744
Anantha <rakrishnan14@gmail.com>
2018-01-09 22:36:15 -0600

    Update bubbleSort.js

--- bubbleSort.js
+++ bubbleSort.js
@@ -1,17 +1,17 @@
-var arr = [11,10,9,8,7,6,5,4,3,2,1];
-
-for (var j = 0; j < arr.length ; j++){
-  for(var i = 0; i < arr.length - 1; i++){ 
+function bubbleSort(arr){
+  var temp;
   
-    if(arr[i] > arr[i+1]){
-      var temp = arr[i]; 
-      arr[i] = arr[i+1]; // 2
-      arr[i+1] = temp; // 11
-    
+  for(var j = 0; j < arr.length; j++){   
+    for(var i = 0; i < arr.length - 1;i++){
+      if(arr[i] > arr[i+1]){
+         temp = arr[i];
+         arr[i] = arr[i + 1];
+         arr[i+1] = temp
+                 
+      }    
+    }
   }
-}
-  console.log(arr);
   
+  console.log(arr)
 }
-
 
